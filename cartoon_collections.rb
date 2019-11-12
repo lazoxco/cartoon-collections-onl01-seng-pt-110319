474708fb582d783e5f4ba1aa187cafee885e4e7a
Nory Lazo <nlazo@icloud.com>
2019-11-12 06:21:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.map()
+  planeteer_calls.map { |element| }
 end
 
 def long_planeteer_calls# code an argument here
