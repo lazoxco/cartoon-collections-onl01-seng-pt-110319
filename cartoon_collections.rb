11802581e000a08856433131a1ef3219f00c0a45
Nory Lazo <nlazo@icloud.com>
2019-11-12 06:45:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.any? { |word| }
+  calls.any? { |word| word.length == 4 }
 end
 
 def find_the_cheese# code an argument here
